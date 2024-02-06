10d885ef5677e52f841f2c484df2a7dfec340fd1
Megan Walford <mwalford22@gmail.com>
2024-02-06 15:27:13 +0000

    connecting python to the chinook file

---
+++ sql-expression.py
@@ -0,0 +1,47 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+# executing the instructions from our localhost "chinook" db
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db)
+
+# create variable for "Artist" table
+artist_table = Table(
+    "artist", meta,
+    Column("artist_id", Integer, primary_key=True),
+    Column("name", String)
+)
+
+# create variable for "Album" table
+album_table = Table(
+    "album", meta,
+    Column("album_id", Integer, primary_key=True),
+    Column("title", String),
+    Column("artist_id", Integer, ForeignKey("artist_table.artist_id"))
+)
+
+# create variable for "Track" table
+track_table = Table(
+    "track", meta,
+    Column("track_id", Integer, primary_key=True),
+    Column("name", String),
+    Column("album_id", Integer, ForeignKey("album_table.AlbumId")),
+    Column("media_type_id", Integer, primary_key=False),
+    Column("genre_id", Integer, primary_key=False),
+    Column("composer", String),
+    Column("milliseconds", Integer),
+    Column("bytes", Integer),
+    Column("unit_price", Float)
+)
+
+# making the connection
+with db.connect() as connection:
+
+    # Query 1 - select all records from the "Artist" table
+    select_query = artist_table.select()
+
+    results = connection.execute(select_query)
+    for result in results:
+        print(result)
