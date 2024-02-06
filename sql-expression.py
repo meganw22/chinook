f3d909d4b72787173ce3b92ae055d77e05e7cd7d
Megan Walford <mwalford22@gmail.com>
2024-02-06 15:39:02 +0000

    Running of basic queries completed

--- sql-expression.py
+++ sql-expression.py
@@ -27,7 +27,7 @@ track_table = Table(
     "track", meta,
     Column("track_id", Integer, primary_key=True),
     Column("name", String),
-    Column("album_id", Integer, ForeignKey("album_table.AlbumId")),
+    Column("album_id", Integer, ForeignKey("album_table.album_id")),
     Column("media_type_id", Integer, primary_key=False),
     Column("genre_id", Integer, primary_key=False),
     Column("composer", String),
@@ -40,7 +40,22 @@ track_table = Table(
 with db.connect() as connection:
 
     # Query 1 - select all records from the "Artist" table
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
+
+    # Query 2 - select only the "Name" column from the "Artist" table
+    # select_query = artist_table.select().with_only_columns([artist_table.c.name])
+
+    # Query 3 - select only 'Queen' from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.name == "Queen")
+
+    # Query 4 - select only by 'artist_id' #51 from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.artist_id == 51)
+
+    # Query 5 - select only the albums with 'artist_id' #51 on the "Album" table
+    # select_query = album_table.select().where(album_table.c.artist_id == 51)
+
+    # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
+    select_query = track_table.select().where(track_table.c.composer == "Black Sabbath")
 
     results = connection.execute(select_query)
     for result in results:
